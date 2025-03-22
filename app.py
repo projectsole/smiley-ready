46cd913fcb89f2b1b93783c9eceb7a8b97d2dc15
smelly <68517851+projectsole@users.noreply.github.com>
2025-03-22 11:36:55 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/menu/piazza")
+def menu_piazza():
+    return render_template("menu_piazza.html")
+
+@app.route("/menu/qbio")
+def menu_qbio():
+    return render_template("menu_qbio.html")
+
+@app.route("/recensioni/gramsci")
+def recenioni_gramsci():
+    return render_template("index_review.html")
+
+@app.route("/suggerisci/gramsci")
+def suggest_gramsci():
+    return render_template("index_suggest.html")
+
+@app.route("/allergeni")
+def allergeni():
+    return render_template("index_allergeni.html")
+
+
+def handler(request):
+    return app.wsgi_app
